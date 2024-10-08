cb720d14f26b4e797e24c5952f0c35723b96fb0f
lalagum <maksmadeng@gmail.com>
2024-10-08 16:31:27 +0200

    fix weiter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-<<<<<<< HEAD
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#ff6347',
-        secondary: '#4caf50',
-      },
-    },
-    plugins: [],
-  }
-}
-=======
     "./src/**/*.{js,jsx,ts,tsx}", // Tailwind 要监视的文件
   ],
   theme: {
@@ -24,4 +10,3 @@ export default {
 
 }
 
->>>>>>> 460db07 (Router and Tailwind css basic)
